5f3e0a8f727479a86e3e712e46a1e7e445f9c9c2
João Andrade <joaovictorvieira.23@hotmail.com>
2022-08-19 16:05:41 -0300

    feat: created category component

---
+++ components/Categories.tsx
@@ -0,0 +1,12 @@
+import { ScrollView, Text } from 'react-native'
+import React from 'react'
+
+const Categories = () => {
+  return (
+    <ScrollView>
+      <Text>Categories Hello</Text>
+    </ScrollView>
+  )
+}
+
+export default Categories
