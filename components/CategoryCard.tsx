8da3f982a004634bb5fcd8e08d1fe8d15d7aba0b
João Andrade <joaovictorvieira.23@hotmail.com>
2022-08-19 16:37:06 -0300

    feat: create category card component

---
+++ components/CategoryCard.tsx
@@ -0,0 +1,22 @@
+import { Image, Text, TouchableOpacity } from 'react-native'
+import React from 'react'
+
+interface PropsCategoryCard {
+    imgUrl: string;
+    title: string;
+}
+
+const CategoryCard = ({ imgUrl, title }: PropsCategoryCard) => {
+  return (
+    <TouchableOpacity className="mr-2">
+      <Image source={{
+          uri: imgUrl,
+      }} 
+        className="h-20 w-20 rounded"
+       />
+      <Text className='absolute bottom-1 left-1 text-white font-bold'>{title}</Text>
+    </TouchableOpacity>
+  )
+}
+
+export default CategoryCard
